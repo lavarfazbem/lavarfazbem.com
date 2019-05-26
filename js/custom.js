b93c67b672fefa4353812d5839a0fd463e3dc4c0
Zoyo <lucasoliveiraoueiroz@gmail.com>
2019-05-26 19:37:48 -0300

    Inclusão do Ajax do Formsquash e alter ação de resumo e ites relacionados a serviços;

--- js/custom.js
+++ js/custom.js
@@ -485,31 +485,24 @@ jQuery(document).ready(function(){
 /*----------------------------------------------------*/
     $("#contact").submit(function (e) {
         e.preventDefault();
-        var name = $("#name").val();
-        var email = $("#email").val();
-        var subject = $("#subject").val();
-        var text = $("#text").val();
-        var dataString = 'name=' + name + '&email=' + email + '&subject=' + subject + '&text=' + text;
-        
+        var nome = $("#nome").val();
+        var telefone = $("#telefone").val();
+        var endereco = $("#endereco").val();
+        var mensagem = $("#mensagem").val();
+        var dataString = 'nome=' + nome + '&telefone=' + telefone + '&endereco=' + endereco + '&mensagem=' + mensagem;
 
-        function isValidEmail(emailAddress) {
-            var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-            return pattern.test(emailAddress);
-        };
-
-        if (isValidEmail(email) && (text.length > 100) && (name.length > 1)) {
+        if ((mensagem.length > 0) && (nome.length > 1)) {
             $.ajax({
                 type: "POST",
-                url: "ajax/process.php",
+                url: "https://formsquash.io/f/kZsPUsrjajDiZqbVamAM",
                 data: dataString,
                 success: function () {
-                    $('.success').fadeIn(1000).delay(3000).fadeOut(1000);
+                    $('.success').fadeIn(1000).delay(60000).fadeOut(1000);
                     $('#contact')[0].reset();
                 }
             });
         } else {
-            $('.error').fadeIn(1000).delay(5000).fadeOut(1000);
-
+            $('.error').fadeIn(1000).delay(60000).fadeOut(1000);
         }
 
         return false;
